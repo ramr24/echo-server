aacb4fb34007a3fab4387df069ee22f779d1460b
Ramkumar Rajanbabu <kumar.rajanbabu@gmail.com>
2020-05-05 19:42:18 -0700

    still need to finish one last thing at line 76

--- echo_server.py
+++ echo_server.py
@@ -1,3 +1,5 @@
+# echo_server.py
+
 import socket
 import sys
 import traceback
@@ -5,18 +7,20 @@ import traceback
 
 def server(log_buffer=sys.stderr):
     # set an address for our server
-    address = ('127.0.0.1', 10000)
+    HOST = '127.0.0.1'
+    PORT = 10000
+    address = (HOST, PORT)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4 = AF_INET, TCP = STREAM
 
-     #STILL HAVE TO DO IT ******
     # TODO: You may find that if you repeatedly run the server script it fails,
     #       claiming that the port is already used.  You can set an option on
     #       your socket that will fix this problem. We DID NOT talk about this
     #       in class. Find the correct option by reading the very end of the
     #       socket library documentation:
     #       http://docs.python.org/3/library/socket.html#example
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
@@ -24,7 +28,7 @@ def server(log_buffer=sys.stderr):
     # TODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
     sock.bind(address)
-    sock.listen(1)
+    sock.listen(1) # double check if needed
 
     try:
         # the outer loop controls the creation of new connection sockets. The
@@ -51,15 +55,13 @@ def server(log_buffer=sys.stderr):
                     #       following line with your code.  It's only here as
                     #       a placeholder to prevent an error in string
                     #       formatting
-                    
-                    # STOP HERE *********
-                    data = conn.recv(16)
-
+                    data = conn.recv(16) # (number of bytes)
                     print('received "{0}"'.format(data.decode('utf8')))
                     
                     # TODO: Send the data you received back to the client, log
                     # the fact using the print statement here.  It will help in
                     # debugging problems.
+                    conn.sendall(data)
                     print('sent "{0}"'.format(data.decode('utf8')))
                     
                     # TODO: Check here to see whether you have received the end
@@ -70,6 +72,9 @@ def server(log_buffer=sys.stderr):
                     # message is a trick we learned in the lesson: if you don't
                     # remember then ask your classmates or instructor for a clue.
                     # :)
+
+                    # STILL NEED TO DO ********** and figure out
+
             except Exception as e:
                 traceback.print_exc()
                 sys.exit(1)
@@ -77,16 +82,14 @@ def server(log_buffer=sys.stderr):
                 # TODO: When the inner loop exits, this 'finally' clause will
                 #       be hit. Use that opportunity to close the socket you
                 #       created above when a client connected.
-                print(
-                    'echo complete, client connection closed', file=log_buffer
-                )
+                conn.close()
+                print('echo complete, client connection closed', file=log_buffer)
 
     except KeyboardInterrupt:
         # TODO: Use the python KeyboardInterrupt exception as a signal to
         #       close the server socket and exit from the server function.
         #       Replace the call to `pass` below, which is only there to
         #       prevent syntax problems
-        pass
         print('quitting echo server', file=log_buffer)
 
 
